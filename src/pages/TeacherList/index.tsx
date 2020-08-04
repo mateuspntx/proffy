884bcd4e0935da43118371c2681a3f2ff3a41160
mateuspl <mateuspntx@gmail.com>
2020-08-04 15:48:25 -0300

    TeacherList and PageHeader changed

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -8,8 +8,27 @@ function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
-                <h1>Olá</h1>
+                <form id="search-teachers">
+                    <div className="input-block">
+                        <label htmlFor="subject">Matéria</label>
+                        <input type="text" id="subject"/>
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="week_day">Dia da semana</label>
+                        <input type="text" id="week_day"/>
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="time">Hora</label>
+                        <input type="text" id="time"/>
+                    </div>
+                </form>
             </PageHeader>
+
+            <main>
+                
+            </main>
         </div>
     );
 }
