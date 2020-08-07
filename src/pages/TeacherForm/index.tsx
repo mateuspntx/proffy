d3e45d0edd08a723604bfc2a7c466de24995e25f
mateuspntx <mateuspntx@gmail.com>
2020-08-07 01:56:12 -0300

    feat: Select component added

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 
 import warningIcon from '../../assets/images/icons/warning.svg';
 
@@ -28,7 +29,23 @@ function TeacherForm() {
                 <fieldset>
                     <legend>Sobre a aula</legend>
 
-                    <Input name="subject" label="Matéria" />
+                    <Select 
+                        name="subject" 
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Ciências', label: 'Ciências' },
+                            { value: 'Química', label: 'Química' },
+                            { value: 'Educação Física', label: 'Educação Física' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'História', label: 'História' },
+                        ]}
+                    />
+
                     <Input name="cost" label="Custo da sua hora por aula" />
                     
                 </fieldset>
